2dea8193c6c2deb8aba4aa1bba0b4679ce72f1b8
fcs-creator <y-nishikawa@asobism.co.jp>
2025-03-17 21:57:05 +0900

    ガードと武器の接触時の処理を移行

--- Assets/Library/GameSystem/Guard.cs
+++ Assets/Library/GameSystem/Guard.cs
@@ -19,6 +19,7 @@ public class Guard : MonoBehaviour
 
     //ガードの実体
     GameObject instance;
+    public GameObject Instance { get { return instance; } }
 
     public void SetOwner(Monster monster)
     {
@@ -72,53 +73,7 @@ public class Guard : MonoBehaviour
         await Task.Delay((int)(sec * 1000), canceler.Token);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        GameObject obj = other.gameObject;
-
-        //ガードと接触した時の処理
-        if (obj.CompareTag(Tags.Weapon))
-        {
-            if (HasComponent<Weapon>(obj))
-            {
-                Weapon weapon = obj.GetComponent<Weapon>();
-
-                if (weapon.Owner != Owner)
-                {
-                    weapon.Owner.IsStunned = true;
-
-                    //ガードエフェクトの再生
-                    PlayGuardVFX(obj, other);
-
-                    //武器の所有者を吹き飛ばす方向を計算
-                    Vector2 direction = (weapon.Owner.transform.position - transform.position).normalized;
-                    weapon.Owner.GetComponent<Rigidbody2D>().AddForce(direction * weapon.Damage * Parameters.GUARD_FORCE_SCALE, ForceMode2D.Impulse);
-
-                    //パリィ音を再生
-                    AudioManager.Instance.PlaySE(Parameters.SE_PARRY);
-
-                    //スタン状態を有効にする
-                    weapon.Owner.IsStunned = true;
-
-                    Debug.Log("Guard Hit->Stun Flag On !!!!");
-                }
-            }
-        }
-    }
-
-    //ガードヒットエフェクトの再生
-    void PlayGuardVFX(GameObject weapon, Collider2D weaponCollider)
-    {
-        // 衝突点を取得
-        Vector3 collisionPoint = weaponCollider.ClosestPoint(transform.position);
-
-        // 自分の位置を基準にして衝突の法線ベクトルを計算
-        Vector3 hitNormal = (weapon.transform.position - transform.position).normalized;
-        Quaternion rotation = Quaternion.LookRotation(hitNormal);
-
-        // ガードエフェクトの再生
-        VFXManager.Instance.Play(VFX.Guard, collisionPoint, rotation);
-    }
+    
 
     bool HasComponent<T>(GameObject obj) where T : Component
     {
--- Assets/Library/GameSystem/Weapon.cs
+++ Assets/Library/GameSystem/Weapon.cs
@@ -517,7 +517,50 @@ public class Weapon : MonoBehaviour
         }
     }
 
-    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject obj = other.gameObject;
+
+        //ガードと接触した時の処理
+        if (obj.CompareTag(Tags.Guard))
+        {
+            var guard = obj.transform.parent.GetComponent<Guard>();
+
+            if (guard.Owner != Owner)
+            {
+                Owner.IsStunned = true;
+
+                //ガードエフェクトの再生
+                PlayGuardVFX(obj, other);
+
+                //武器の所有者を吹き飛ばす方向を計算
+                Vector2 direction = (Owner.transform.position - guard.Instance.transform.position).normalized;
+                Owner.GetComponent<Rigidbody2D>().AddForce(direction * Damage * Parameters.GUARD_FORCE_SCALE, ForceMode2D.Impulse);
+
+                //パリィ音を再生
+                AudioManager.Instance.PlaySE(Parameters.SE_PARRY);
+
+                //スタン状態を有効にする
+                Owner.IsStunned = true;
+
+                Debug.Log("Guard Hit->Stun Flag On !!!!");
+            }
+        }
+    }
+
+    //ガードヒットエフェクトの再生
+    void PlayGuardVFX(GameObject weapon, Collider2D weaponCollider)
+    {
+        // 衝突点を取得
+        Vector3 collisionPoint = weaponCollider.ClosestPoint(transform.position);
+
+        // 自分の位置を基準にして衝突の法線ベクトルを計算
+        Vector3 hitNormal = (weapon.transform.position - transform.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(hitNormal);
+
+        // ガードエフェクトの再生
+        VFXManager.Instance.Play(VFX.Guard, collisionPoint, rotation);
+    }
 
     //自分のBodyの当たり判定から1度出た武器は当たるようになる
     private void OnTriggerExit2D(Collider2D other)
